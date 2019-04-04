fe8a1e99baa83c5268641eebefbd07e08a641cc6
Muhammad Riza <riza.riza69@gmail.com>
2019-04-04 17:02:09 +0700

    project javascript

---
+++ index.js
@@ -0,0 +1,17 @@
+"use strict"
+
+let helloWorld = "hello worlds!"
+console.log(helloWorld);
+
+
+let name = 'didi';
+let work = 'Accountant';
+let age = 25;
+
+console.log(`${name} to be a ${work}`);
+console.log(`${name} is ${age} years old`)
+
+alert(`${name} is ${age} years old`);
+
+
+
